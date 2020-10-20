0cec8ab248b73c680fbdc2c033800d4702439908
Zane <zanebgriffin@gmail.com>
2020-10-19 21:22:39 -0500

    adjusted connectiona and server

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,12 @@
+require("dotenv").config()
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/cookbooks_db', { useNewUrlParser: true })
-
+const { MONGODBURI } = process.env;
+const config = { useUnifiedTopology: true, useNewUrlParser: true };
+mongoose.connect(MONGODBURI, config)
+const DB = mongoose.connection;
 mongoose.Promise = Promise
-
+DB.on("open", () => console.log("You are connected to Mongo"))
+  .on("close", () => console.log("You are disconnected to Mongo"))
+  .on("error", (err) => console.log(err));
 
 module.exports = mongoose
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const logger = require('morgan')
+const {PORT} = process.env
 app.use(logger('dev'))
 
 // Add the middleware code needed to accept incoming data and add it to req.body
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({ hello: "Hello World!" });
+  });
 
 const cookbookRouter = require('./controllers/cookbookRoutes')
 app.use('/api/cookbooks/', cookbookRouter)
@@ -13,4 +19,4 @@ app.use('/api/cookbooks/', cookbookRouter)
 const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
 
-app.listen(4000, () => console.log('Server running on port 4000!'))
+app.listen(PORT, () => console.log('Server running on port', PORT))
